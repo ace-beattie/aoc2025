b2d59375552159be2bfb928a374a73e7138b0fc8
Ace Beattie <acebeattie@gmail.com>
2025-12-24 14:21:50 -0700

    Day 3 Part 2

--- aoc2025-3/bin/main.ml
+++ aoc2025-3/bin/main.ml
@@ -4,30 +4,40 @@ let banks =
   puzzle_input |> String.split_on_char '\n'
   |> List.filter (fun s -> String.length s > 0)
 
+type digit_info = { value : int; index : int }
+
+(** Find the largest digit in a string [bank] *)
 let rec find_largest_digit ?(init_index = 0)
-    ?(current_largest = (Int.min_int, 0)) bank stop_at =
-  if init_index >= stop_at then current_largest
+    ?(current_largest = { value = Int.min_int; index = 0 }) bank =
+  if init_index >= String.length bank then current_largest
   else
     let integer_value = Char.code bank.[init_index] - 48 in
-    if integer_value > fst current_largest then
+    if integer_value > current_largest.value then
       find_largest_digit ~init_index:(init_index + 1)
-        ~current_largest:(integer_value, init_index)
-        bank stop_at
-    else
-      find_largest_digit ~init_index:(init_index + 1) ~current_largest bank
-        stop_at
+        ~current_largest:{ value = integer_value; index = init_index }
+        bank
+    else find_largest_digit ~init_index:(init_index + 1) ~current_largest bank
 
-let solve_bank bank =
-  let largest_ten = find_largest_digit bank (String.length bank - 1) in
-  let largest_one =
-    find_largest_digit
-      ~init_index:(snd largest_ten + 1)
-      bank (String.length bank)
-  in
-  string_of_int (fst largest_ten) ^ string_of_int (fst largest_one)
-  |> int_of_string
+(** Solve a single bank by finding the largest 12-digit number that can be
+    formed.
+
+    Recursively finds largest possible digit from left-to-right (allowing room
+    for the remaining digits) *)
+let rec solve_bank bank digits =
+  if digits = 0 then ""
+  else
+    let largest =
+      find_largest_digit (String.sub bank 0 (String.length bank - digits + 1))
+    in
+    string_of_int largest.value
+    ^ solve_bank
+        (String.sub bank (largest.index + 1)
+           (String.length bank - (largest.index + 1)))
+        (digits - 1)
 
 let rec solve_all banks =
-  match banks with [] -> 0 | bank :: rest -> solve_bank bank + solve_all rest
+  match banks with
+  | [] -> 0
+  | bank :: rest -> int_of_string (solve_bank bank 12) + solve_all rest
 
 let () = print_endline ("Total: " ^ string_of_int (solve_all banks))
